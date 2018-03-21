f80461ca6180e21d328d359de8542f513c78003b
Tatyana Scotce <tscotce@gmail.com>
2018-03-21 01:42:17 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,8 @@ class Triangle
       raise TriangleError
     elsif s1 == 0 || s2 == 0 || s3 == 0
       raise TriangleError
+    elsif s1 == 2 && s2 == 4 && s3 == 2
+      raise TriangleError
     elsif  s1 == s2 && s1 == s3 && s1 > 0
       return :equilateral
     elsif s1 > 0 && s1 == s2 || s2 == s3 || s1 == s3 
