98f4dc94bc6d6972156e07db3313adc54b54ace9
Tatyana Scotce <tscotce@gmail.com>
2018-03-21 01:14:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,9 +3,14 @@ class Triangle
   attr_accessor :s1, :s2, :s3, :equilateral, :isosceles, :scalene
   
   def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
   
   def kind
+    if s1 == s2 && s1 == s3
+      return :equilateral
   end
   
 end
