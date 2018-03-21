5db6d209d83a17153824130c273597131d87eb60
Tatyana Scotce <tscotce@gmail.com>
2018-03-21 01:25:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,10 +16,7 @@ class Triangle
     elsif s1 != s2 && s2 != s3 && s1 != s3 
       return :scalene
     elsif s1 == 0 || s2 == 0 || s3 == 0 
-    begin raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-    end
+    raise TriangleError
     end
   end
   
