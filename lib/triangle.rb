140c7c32564f75fa6dc9ec9490253f2dc0b8b689
Tatyana Scotce <tscotce@gmail.com>
2018-03-21 01:28:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if s1 == s2 && s1 == s3 && s1 > 0
       return :equilateral
-    elsif s1 == s2 || s2 == s3 || s1 == s3 && s1 != 0 && s1 > 0
+    elsif s1 == s2 || s2 == s3 || s1 == s3 && s1 > 0
       return :isosceles
     elsif s1 != s2 && s2 != s3 && s1 != s3 && s1 > 0 && s2 > 0 && s3 > 0
       return :scalene
